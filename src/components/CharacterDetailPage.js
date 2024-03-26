c5fa6fde6da0d4fbf2464612022c9a164362650d
Meya <gabzmejia117@gmail.com>
2024-03-26 12:59:55 +0800

    added img path construction for talents passives and constellation character detail page is now in rc1 status todo: add more builds for characters

--- src/components/CharacterDetailPage.js
+++ src/components/CharacterDetailPage.js
@@ -105,18 +105,9 @@ const CharacterDetailPage = () => {
           console.error('Error fetching artifact data:', error);
         }
       };
-      
-      
-      
-      
-      
-      
-      
-    
       fetchArtifactData();
-    }, [characterBuilds, characterName]);
+    }, [ characterName ]);
     
-  
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -186,18 +177,182 @@ const getSpecialCaseImageSrc = (characterName) => {
       return `UI_Gacha_AvatarImg_${characterName.replace(/\s+/g, '')}`;
   }
 };
+
   // Function to get the material image source based on ID
   const getMaterialImageSrc = (materialId) => {
     return `/assets/materials/UI_ItemIcon_${materialId}.png`;
   };
   
+  const convertCharacterNameToImageFormat = (characterName) => {
+    switch (characterName) {
+      case 'Aether':
+        return 'PlayerBoy';
+      case 'Alhaitham':
+        return 'Alhatham';
+      case 'Amber':
+        return 'Ambor';
+      case 'Arataki Itto':
+        return 'Itto';
+      case 'Baizhu':
+        return 'Baizhuer';
+      case 'Hu Tao':
+        return 'Hutao';
+      case 'Jean':
+        return 'Qin';
+      case 'Kaedehara Kazuha':
+        return 'Kazuha';
+      case 'Kamisato Ayaka':
+        return 'Ayaka';
+      case 'Kamisato Ayato':
+        return 'Ayato';
+      case 'Kirara':
+        return 'Momoka';
+      case 'Kujou Sara':
+        return 'Sara';
+      case 'Kuki Shinobu':
+        return 'Shinobu';
+      case 'Lumine':
+        return 'PlayerGirl';
+      case 'Lynette':
+        return 'Linette';
+      case 'Lyney':
+        return 'Liney';
+      case 'Noelle':
+        return 'Noel';
+      case 'Raiden Shogun':
+        return 'Shougun';
+      case 'Sangonomiya Kokomi':
+        return 'Kokomi';
+      case 'Shikanoin Heizou':
+        return 'Heizo';
+      case 'Thoma':
+        return 'Tohma';
+      case 'Yae Miko':
+        return 'Yae';
+      case 'Yanfei':
+        return 'Feiyan';
+      case 'Yun Jin':
+        return 'Yunjin';
+      case 'Xianyun':
+        return 'Liuyun';
+      default:
+        // For all other cases, just remove spaces and convert to lowercase
+        return characterName.replace(/\s+/g, '').toLowerCase();
+    }
+  };
+  
+// Helper function to get the talent image based on weapon type and talent type
+const getTalentImage = (weaponType, talentType, characterName) => {
+  const talentTypeMap = {
+    NORMAL_ATTACK: "Skill_A",
+    ELEMENTAL_SKILL: "Skill_S",
+    ELEMENTAL_BURST: "Skill_E"
+  };
+
+  // Convert character name to match image format
+  const characterNameForImage = convertCharacterNameToImageFormat(characterName);
+
+  let talentImagePrefix;
+  if (talentType === "ELEMENTAL_SKILL") {
+    // Elemental Skill talent image
+    talentImagePrefix = "Skill_S";
+    const imagePath = `/assets/talents/${talentImagePrefix}_${characterNameForImage}_01.png`;
+    return imagePath;
+  } else if (talentType === "ELEMENTAL_BURST") {
+    // Elemental Burst talent image
+    talentImagePrefix = "Skill_E";
+    const imagePath = `/assets/talents/${talentImagePrefix}_${characterNameForImage}_01_HD.png`;
+    return imagePath;
+  }
+
+  // For Normal Attack talent image
+  let weaponPrefix;
+  switch (weaponType) {
+    case "sword":
+      weaponPrefix = "01";
+      break;
+    case "bow":
+      weaponPrefix = "02";
+      break;
+    case "polearm":
+      weaponPrefix = "03";
+      break;
+    case "claymore":
+      weaponPrefix = "04";
+      break;
+    case "catalyst":
+      return `/assets/talents/Skill_A_Catalyst_MD.png`;
+    default:
+      weaponPrefix = "01"; // Default to sword if weapon type is unknown
+  }
+
+  return `/assets/talents/${talentTypeMap[talentType]}_${weaponPrefix}.png`;
+};
+
+const getPassiveTalentImage = (characterName, talent) => {
+  if (!talent) {
+    return ""; // Return default image URL or handle it as needed
+  }
+
+  // Check unlock condition
+  if (talent.unlock === "Unlocked at Ascension 1") {
+    return `/assets/talents/UI_Talent_S_${convertCharacterNameToImageFormat(characterName)}_05.png`;
+  } else if (talent.unlock === "Unlocked at Ascension 4") {
+    return `/assets/talents/UI_Talent_S_${convertCharacterNameToImageFormat(characterName)}_06.png`;
+  } else {
+    // Check description for additional conditions
+    if (talent.description.includes("Decreases all party members' gliding Stamina Consumption")) {
+      return "/assets/talents/UI_Talent_Explosion_Glide.png";
+    } else if (talent.description.includes("Weapon Ascension Materials, he has a 10% chance to receive double the product.")) {
+      return "/assets/talents/UI_Talent_Combine_Weapon_Double.png";
+    }
+    
+    // For other unlock conditions, handle them accordingly
+    // You can add more conditions here as needed
+    return ""; // Return default image URL or handle it as needed
+  }
+};
+
+// Define function to get constellation image URL
+const getConstellationImage = (characterName, constellation) => {
+  const constellationIndex = constellation.level - 1; // Adjust level to array index
+  let imageUrl;
+
+  // Ensure consistent character name formatting
+  const formattedCharacterName = convertCharacterNameToImageFormat(characterName);
+
+  // Construct image URL based on constellation type and index
+  switch (constellationIndex) {
+      case 0:
+          imageUrl = `/assets/constellations/UI_Talent_S_${formattedCharacterName}_01.png`; // 1st constellation
+          break;
+      case 1:
+          imageUrl = `/assets/constellations/UI_Talent_S_${formattedCharacterName}_02.png`; // 2nd constellation
+          break;
+      case 2:
+          imageUrl = `/assets/constellations/UI_Talent_U_${formattedCharacterName}_01.png`; // 3rd constellation
+          break;
+      case 3:
+          imageUrl = `/assets/constellations/UI_Talent_S_${formattedCharacterName}_03.png`; // 4th constellation
+          break;
+      case 4:
+          imageUrl = `/assets/constellations/UI_Talent_U_${formattedCharacterName}_02.png`; // 5th constellation
+          break;
+      case 5:
+          imageUrl = `/assets/constellations/UI_Talent_S_${formattedCharacterName}_04.png`; // 6th constellation
+          break;
+      default:
+          imageUrl = ''; // Default image URL
+  }
+  return imageUrl;
+};
 
   return (
     <div className="character-detail-wrapper">
       <NavMenu />
       <div className="character-detail-container">
       <h2>{character.name}</h2>
-        <div className="character-header">
+      <div className="character-header" style={{ backgroundImage: `url('/assets/constellations/Eff_UI_Talent_${convertCharacterNameToImageFormat(character.name)}.png')` }}>
           <img src={`/assets/characters/${getSpecialCaseImageSrc(characterName)}.png`} alt={characterName} />
           <div className="character-info">
           <div className="info-item">
@@ -217,7 +372,8 @@ const getSpecialCaseImageSrc = (characterName) => {
             <p>{character.weapon}</p>
           </div>
           <div className="info-item">
-            <h3>Nation:<p>{character.nation}</p></h3>
+            <h3>Nation:</h3>
+            <p>{character.nation}</p>
           </div>
           <div className="info-item">
             <h3>Affiliation:</h3>
@@ -264,14 +420,9 @@ const getSpecialCaseImageSrc = (characterName) => {
         <h5>{setName}</h5>
         <ul>
           {setItems.map((artifact, index) => {
-            console.log("setName:", setName);
-            console.log("artifact:", artifact);
             const artifactName = artifact.trim(); // Remove any leading/trailing spaces
-            console.log("artifactName:", artifactName);
             const matchedArtifact = artifacts.find(item => item.name === artifactName);
-            console.log("matchedArtifact:", matchedArtifact);
             if (!matchedArtifact) {
-              console.log(`Artifact not found for name: ${artifactName}`);
               return null; // Skip rendering if artifact is not found
             }
             return (
@@ -309,46 +460,45 @@ const getSpecialCaseImageSrc = (characterName) => {
                   ))}
                 </ul>
               </li>
-
-              {/* Display talent priority */}
-              <li>
-                <h4>Talent Priority</h4>
-                <ul>
-                  {characterBuilds[characterName].talent_priority.map((talent, index) => (
-                    <li key={index}>{index + 1}: {talent}</li>
-                  ))}
-                </ul>
-              </li>
             </ul>
           </div>
         )}
-        <div className="character-section">
-          <h3>Talents:</h3>
-          {character.skillTalents && character.skillTalents.map((talent) => (
-            <div key={talent.name} className="talent">
-              <h4>{talent.name}</h4>
-              <p>{talent.description}</p>
-            </div>
-          ))}
+<div className="character-section">
+  <h3>Talents:</h3>
+  {character.skillTalents && character.skillTalents.map((talent, index) => (
+    <div key={index} className="talent">
+      <h4>{talent.name}</h4>
+      {characterName && (
+        <img src={getTalentImage(character.weapon, talent.type, convertCharacterNameToImageFormat(characterName))} alt={talent.type} />
+      )}
+      <p>{talent.description}</p>
+    </div>
+  ))}
+</div>
+
+<div className="character-section">
+  <h3>Passive Talents:</h3>
+  {character.passiveTalents && character.passiveTalents.map((talent, index) => (
+    <div key={index} className="passive-talent">
+      <h4>{talent.name}</h4>
+      {/* Get corresponding passive talent image */}
+      <img src={getPassiveTalentImage(character.name, talent)} alt={talent.name} />
+      <p>{talent.description}</p>
+    </div>
+  ))}
+</div>
+
+<div className="character-section">
+    <h3>Constellations:</h3>
+    {character.constellations && character.constellations.map((constellation) => (
+        <div key={constellation.name} className="constellation">
+            <h4>{constellation.name}</h4>
+            {/* Get corresponding constellation image */}
+            <img src={getConstellationImage(character.name, constellation)} alt={constellation.name} />
+            <p>{constellation.description}</p>
         </div>
-        <div className="character-section">
-          <h3>Passive Talents:</h3>
-          {character.passiveTalents && character.passiveTalents.map((talent) => (
-            <div key={talent.name} className="passive-talent">
-              <h4>{talent.name}</h4>
-              <p>{talent.description}</p>
-            </div>
-          ))}
-        </div>
-        <div className="character-section">
-          <h3>Constellations:</h3>
-          {character.constellations && character.constellations.map((constellation) => (
-            <div key={constellation.name} className="constellation">
-              <h4>{constellation.name}</h4>
-              <p>{constellation.description}</p>
-            </div>
-          ))}
-        </div>
+    ))}
+</div>
         <div className="character-section">
           <h3>Ascension Costs:</h3>
           {character && character.costs && Object.entries(character.costs).map(([ascendLevel, items]) => (
