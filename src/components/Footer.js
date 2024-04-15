ea88c5db30a4c822a0410497a9c70920371a6d72
Meya <gabzmejia117@gmail.com>
2024-04-15 12:11:12 +0800

    feat: new footer, new nav, font redesign, character splash placement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,44 @@
-// Footer.js
-
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../styles/Footer.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Footer = () => {
   return (
-    <footer className="Footer">
-      <div className="footer-links">
-        <Link to="/terms-of-use">Terms of Use</Link>
-        <Link to="/cookies-policy">Cookies Policy</Link>
-        <Link to="/privacy-policy">Privacy Policy</Link>
-        <Link to="/about-us">About Us</Link>
-        <Link to="/contact">Contact</Link>
-      </div>
-      <p className="disclaimer">
-
-      GenshinBud is an independent platform and is not affiliated or associated with miHoYo/HoYoverse company. Instead, it operates as a dedicated resource for guides and character builds tailored to the game Genshin Impact. Our mission is to assist players by offering reliable and precise information to enhance their gaming experience.
-      </p>
-      <p className="copyright">© 2023 GENSHIN BUD.</p>
-    </footer>
+    <section>
+      <footer className="top">
+        <img src="/assets/icons/logo.png" alt="Logo" />
+        <div className="links">
+          <div className="links-column">
+          <h2>Resources</h2>
+            <a href="#">API</a>
+            <a href="#">Data References</a>
+            <a href="#">Github Repo</a>
+            <a href="#">License</a>
+            <a href="#">Privacy Policy</a>
+          </div>
+          <div className="links-column">
+          <h2>Disclaimer  </h2>
+            <p>        
+            GenshinBuddy is not endorsed by miHoYo Co Ltd. and does not reflect the views or opinions of MiHoyo or anyone officially involved in producing or managing Genshin Impact.
+            </p>
+          </div>
+          <div className="links-column socials-column">
+            <h2>Social Media</h2>
+            <p>
+            If anything is broken for you, feel free to contact me on
+            </p>
+            <div className="socials">
+              <a className="fab fa-discord" href="#"></a>
+              <a className="fab fa-instagram" href="#"></a>
+              <a className="fab fa-twitter" href="#"></a>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="bottom">
+        <p className="copyright">© 2023 GenshinBuddy All rights reserved</p>
+      </footer>
+    </section>
   );
 };
 
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,18 +1,52 @@
 // NavMenu.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/NavMenu.css';
 
 const NavMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    const burgerMenu = document.querySelector(".menu-icon");
+    const src = burgerMenu.getAttribute('src');
+    const iconName = src === '/assets/icons/burger-menu.svg' ? '/assets/icons/close.svg' : '/assets/icons/burger-menu.svg';
+
+    burgerMenu.setAttribute('src', iconName);
+
+    const navigation = document.querySelector('.navigation');
+    navigation.classList.toggle('navigation--mobile');
+
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="NavMenu">
-      <ul>
-        <li><Link to="/">Characters</Link></li>
-        <li><Link to="/teams">Teams</Link></li>
-        <li><Link to="/tierlist">Tier List</Link></li>
-        <li><Link to="/farming">Farming</Link></li>
-      </ul>
+      <div className="menu__wrapper">
+        <div className="menu__bar">
+          <Link to="/" title="Logo">
+            <img
+              className="logo"
+              src="/assets/icons/logo.png"
+              title="Logo"
+              alt="Logo"
+            />
+          </Link>
+          <img
+            className="menu-icon"
+            src={isMenuOpen ? '/assets/icons/close.svg' : '/assets/icons/burger-menu.svg'}
+            title={isMenuOpen ? 'Close Menu' : 'Open Menu'}
+            alt={isMenuOpen ? 'Close Menu' : 'Open Menu'}
+            onClick={toggleMenu}
+          />
+          <ul className={`navigation ${isMenuOpen ? 'navigation--mobile' : ''}`}>
+            <li><Link to="/">Characters</Link></li>
+            <li><Link to="/teams">Teams</Link></li>
+            <li><Link to="/tierlist">Tier List</Link></li>
+            <li><Link to="/farming">Farming</Link></li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
